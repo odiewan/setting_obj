aa00af5c3b80f2bb86fe284448de6f105f401ea5
odiewan <odie@odiesystems.com>
2022-10-01 18:17:40 -0700

    initial dev 00

--- setting_obj.cpp
+++ setting_obj.cpp
@@ -1,8 +1,20 @@
 #include "setting_obj.h"
+#include <String.h>
 
-setting_obj::setting_obj(uint8_t nIdx, String nName, String nDesc, uint8_t nType) {
+//=================================================================================================
+setting_obj::setting_obj() {
+    setIndex = 0;
+    setName = "Default Setting Name";
+    setDesc = "Default Setting Description";
+    setType = SET_TYPE_BOOL;
+    setValue = 0;
+}
+
+//=================================================================================================
+setting_obj::setting_obj(uint8_t nIdx, String nName, String nDesc, uint8_t nType, uint8_t nValue) {
     setIndex = nIdx;
     setName = nName;
     setDesc = nDesc;
     setType = nType;
+    setValue = nValue;
 }
--- setting_obj.h
+++ setting_obj.h
@@ -4,16 +4,24 @@
 #include "Arduino.h"
 #include <String.h>
 
+enum settingTypes {
+    SET_TYPE_BOOL,
+    SET_TYPE_CHAR,
+    SET_TYPE_INT,
+    NUM_SETTING_TYPES
+};
+
 
 class setting_obj {
     public:
         uint8_t setIndex; //--index to 512 x 1 byte nvram space
         String setName;
         String setDesc;
-        unit8_t setType; // bool char int
+        uint8_t setType; // bool char int
+        uint8_t setValue;
 
-        setting_obj(uint8_t nIdx, String nName, String nDesc, uint8_t nType);
-}
-
+        setting_obj();
+        setting_obj(uint8_t nIdx, String nName, String nDesc, uint8_t nType, uint8_t nValue);
+};
 
 #endif
