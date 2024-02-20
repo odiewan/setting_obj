adebd01180301d540dc86b6b319a1b34284a13f2
Odie <odie@odiesystems.com>
2024-02-20 14:20:37 -0800

    support ESPFlash

--- setting_obj.cpp
+++ setting_obj.cpp
@@ -10,25 +10,82 @@ setting_obj::setting_obj() {
 }
 
 //=================================================================================================
-setting_obj::setting_obj(String nName, String nDesc, uint8_t nValue) {
+setting_obj::setting_obj(String nName, ESPFlashCounter* nFlshPtr) {
     soName = nName;
+    soFlashCntPtr = nFlshPtr;
+    soDesc = "Default description";
+    soType = SET_TYPE_BOOL;
+    soBool = 0;
+}
+
+//=================================================================================================
+setting_obj::setting_obj(String nName, ESPFlashCounter* nFlshPtr) {
+    soName = nName;
+    soFlashCntPtr = nFlshPtr;
+    soDesc = "Default description";
+    soType = SET_TYPE_CNTR;
+    soCntr = 0;
+}
+
+//=================================================================================================
+setting_obj::setting_obj(String nName, ESPFlashString* nFlshPtr) {
+    soName = nName;
+    soFlashStringPtr = nFlshPtr;
+    soDesc = "Default description";
+    soType = SET_TYPE_SRING;
+    soString = "EMPTY";
+}
+
+//=================================================================================================
+setting_obj::setting_obj(String nName, ESPFlashCounter* nFlshPtr, uint8_t nValue) {
+    soName = nName;
+    soFlashCntPtr = nFlshPtr;
+    soDesc = "Default description";
+    soType = SET_TYPE_BOOL;
+    soBool = nValue;
+}
+
+//=================================================================================================
+setting_obj::setting_obj(String nName, ESPFlashCounter* nFlshPtr, uint8_t nValue) {
+    soName = nName;
+    soFlashCntPtr = nFlshPtr;
+    soDesc = "Default description";
+    soType = SET_TYPE_CNTR;
+    soCntr = nValue;
+}
+
+//=================================================================================================
+setting_obj::setting_obj(String nName, ESPFlashString* nFlshPtr, String nValue) {
+    soName = nName;
+    soFlashStringPtr = nFlshPtr;
+    soDesc = "Default description";
+    soType = SET_TYPE_SRING;
+    soString = nValue;
+}
+
+//=================================================================================================
+setting_obj::setting_obj(String nName, ESPFlashCounter* nFlshPtr, String nDesc, uint8_t nValue) {
+    soName = nName;
+    soFlashCntPtr = nFlshPtr;
     soDesc = nDesc;
     soType = SET_TYPE_BOOL;
     soBool = nValue;
 }
 
 //=================================================================================================
-setting_obj::setting_obj(String nName, String nDesc,  uint16_t nValue) {
-    soName = nName;
-    soDesc = nName;
-    soType = SET_TYPE_CNTR;
-    soCntr = nValue;
+setting_obj::setting_obj(String nName, ESPFlashCounter* nFlshPtr, String nDesc, uint16_t nValue) {
+  soName = nName;
+  soFlashCntPtr = nFlshPtr;
+  soDesc = nName;
+  soType = SET_TYPE_CNTR;
+  soCntr = nValue;
 }
 
 //=================================================================================================
-setting_obj::setting_obj(String nName, String nDesc, String nValue) {
-    soName = nName;
-    soDesc = nName;
-    soType = SET_TYPE_SRING;
-    soString = nValue;
+setting_obj::setting_obj(String nName, ESPFlashString* nFlshPtr, String nDesc, String nValue) {
+  soName = nName;
+  soFlashStringPtr = nFlshPtr;
+  soDesc = nName;
+  soType = SET_TYPE_SRING;
+  soString = nValue;
 }
--- setting_obj.h
+++ setting_obj.h
@@ -2,6 +2,8 @@
 #define SETTING_OBJ_H_
 
 #include <String.h>
+#include "ESPFlashCounter.h"
+#include "ESPFlashString.h"
 #include "Arduino.h"
 
 enum settingTypes {
@@ -20,11 +22,22 @@ class setting_obj {
      uint8_t soBool;
      uint16_t soCntr;
      String soString;
+     ESPFlashCounter* soFlashCntPtr;
+     ESPFlashString* soFlashStringPtr;
 
      setting_obj();
-     setting_obj(String nName, String nDesc, uint8_t nValue);
-     setting_obj(String nName, String nDesc, uint16_t nValue);
-     setting_obj(String nName, String nDesc, String nValue);
+
+     setting_obj(String nName, ESPFlashCounter* nFlshPtr, uint8_t nValue);
+     setting_obj(String nName, ESPFlashCounter* nFlshPtr, uint16_t nValue);
+     setting_obj(String nName, ESPFlashString* nFlshPtr, String nValue);
+
+     setting_obj(String nName, ESPFlashCounter* nFlshPtr);
+     setting_obj(String nName, ESPFlashCounter* nFlshPtr);
+     setting_obj(String nName, ESPFlashString* nFlshPtr);
+
+     setting_obj(String nName, ESPFlashCounter* nFlshPtr, String nDesc, uint8_t nValue);
+     setting_obj(String nName, ESPFlashCounter* nFlshPtr, String nDesc, uint16_t nValue);
+     setting_obj(String nName, ESPFlashString* nFlshPtr, String nDesc, String nValue);
 };
 
 #endif
