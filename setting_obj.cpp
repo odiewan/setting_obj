b59653d2cf6fcc49bd875b633480f4fcbc310d23
Odie <odie@odiesystems.com>
2024-05-18 00:57:45 -0700

    debugging

--- setting_obj.cpp
+++ setting_obj.cpp
@@ -1,127 +1,132 @@
 #include "setting_obj.h"
 
-#include <FS.h>
+// #include <FS.h>
 #include <String.h>
 
 //=================================================================================================
 setting_obj::setting_obj() {
-    soName = "Default Setting Name";
-    soDesc = "Default Setting Description";
+    constructor_core();
+    }
+
+
+
+//===================================================================================================
+setting_obj::setting_obj(String nName, bool nDefValue, bool nReadOnly) {
+    constructor_core();
+    soName = nName;
     soType = SET_TYPE_BOOL;
-    soBool = false;
+    // ESPFlashCounter soEspFlashCounterPtr("/counter");
+    // soEspFlashCounterPtr.set(nDefValue);
+    }
+
+//===================================================================================================
+setting_obj::setting_obj(String nName, uint16_t nDefValue, bool nReadOnly) {
+    constructor_core();
+    soName = nName;
+    soType = SET_TYPE_CNTR;
+    // ESPFlashCounter soEspFlashCounterPtr("/counter");
+    // soEspFlashCounterPtr.set(nDefValue);
+    }
+
+//===================================================================================================
+setting_obj::setting_obj(String nName, String nDefValue, bool nReadOnly) {
+    constructor_core();
+    soName = nName;
+    soType = SET_TYPE_STRING;
+    // ESPFlashString soEspFlashString(String(nDefValue));
+    }
+
+
+// //===================================================================================================
+// setting_obj::setting_obj(String nName, bool nValue, ESPFlashCounter* nEspFlashCntr, bool nReadOnly) {
+//     soName = nName;
+//     soType = SET_TYPE_BOOL;
+//     soEspFlashCounterPtr = soEspFlashCounterPtr;
+// }
+
+// //===================================================================================================
+// setting_obj::setting_obj(String nName, uint16_t nValue, ESPFlashCounter* nEspFlashCntr, bool nReadOnly) {
+//     soName = nName;
+//     soType = SET_TYPE_CNTR;
+//     soEspFlashCounterPtr = soEspFlashCounterPtr;
+// }
+
+// //===================================================================================================
+// setting_obj::setting_obj(String nName, String nValue, ESPFlashString* nEspFlashStr, bool nReadOnly) {
+//     soName = nName;
+//     soType = SET_TYPE_STRING;
+//     soEspFlashString = nEspFlashStr;
+// }
+
+
+//===================================================================================================
+void setting_obj::constructor_core() {
+    soName = "def setting object";
+    soDesc = "setting obj desc";
+    soType = SET_TYPE_BOOL;
     soReadOnly = false;
-}
+    soBool = false;
+    soCntr = 0;
+    soString = "def_str";
+    // SPIFFS.begin();
+
+    }
 
 //=================================================================================================
-setting_obj::setting_obj(String nName, uint8_t nValue, bool nReadOnly) {
-    soName = nName;
-
-    soDesc = "Default description";
-    soType = SET_TYPE_CNTR;
-    soCntr = nValue;
-    soReadOnly = nReadOnly;
-}
-
-//=================================================================================================
-setting_obj::setting_obj(String nName, uint16_t nValue, bool nReadOnly) {
-    soName = nName;
-
-    soDesc = "Default description";
-    soType = SET_TYPE_CNTR;
-    soCntr = nValue;
-    soReadOnly = nReadOnly;
-}
-
-//=================================================================================================
-setting_obj::setting_obj(String nName, String nValue, bool nReadOnly) {
-    soName = nName;
-
-    soDesc = "Default description";
-    soType = SET_TYPE_SRING;
-    soString = nValue;
-    soReadOnly = nReadOnly;
-}
-
-//=================================================================================================
-setting_obj::setting_obj(String nName, String nDesc, uint8_t nValue, bool nReadOnly) {
-    soName = nName;
-
-    soDesc = nDesc;
-    soType = SET_TYPE_BOOL;
+void setting_obj::soSetVal(bool nValue) {
     soBool = nValue;
-    soReadOnly = nReadOnly;
-}
-
-//=================================================================================================
-setting_obj::setting_obj(String nName, String nDesc, uint16_t nValue, bool nReadOnly) {
-  soName = nName;
-
-  soDesc = nName;
-  soType = SET_TYPE_CNTR;
-  soCntr = nValue;
-  soReadOnly = nReadOnly;
-}
-
-//=================================================================================================
-setting_obj::setting_obj(String nName, String nDesc, String nValue, bool nReadOnly) {
-  soName = nName;
-  soDesc = nName;
-  soType = SET_TYPE_SRING;
-  soString = nValue;
-  soReadOnly = nReadOnly;
-}
-
-
-//=================================================================================================
-void setting_obj::soSetVal(uint8_t nValue) {
-    soBool = nValue;
-}
+    // soEspFlashCounterPtr->set(nValue);
+    }
 
 //=================================================================================================
 void setting_obj::soSetVal(uint16_t nValue) {
     soCntr = nValue;
-}
+    // soEspFlashCounterPtr->set(nValue);
+    }
 
- //=================================================================================================
+//=================================================================================================
 void setting_obj::soSetVal(String nValue) {
     soString = nValue;
-}
+    // soEspFlashString->set(nValue);
+    }
 
 //==================================================================================================
-uint8_t setting_obj::getBool() {
-
+bool setting_obj::getBool() {
     return soBool;
+    // return (bool)soEspFlashCounterPtr->get();
 }
 
 //==================================================================================================
 uint16_t setting_obj::getCounter() {
-    return soCntr;
-
+    // return (uint16_t)soEspFlashCounterPtr->get();
+return soCntr;
 }
 
 //==================================================================================================
 String  setting_obj::getString() {
+    // return soEspFlashString->get();
     return soString;
-
 }
 
 //==================================================================================================
 String  setting_obj::getValAsString() {
-    String _tmpStr;
+    String _tmpStr = ":";
 
     switch (soType) {
         case SET_TYPE_BOOL:
-            _tmpStr = String(soBool);
+            _tmpStr += (String)soBool;
             break;
         case SET_TYPE_CNTR:
-            _tmpStr = String(soCntr);
+            _tmpStr += (String)soCntr;
             break;
-        case SET_TYPE_SRING:
-            _tmpStr = soString;
+        case SET_TYPE_STRING:
+            _tmpStr += soString;
             break;
 
         default:
+            _tmpStr += "d";
             break;
     }
     return _tmpStr;
+
 }
--- setting_obj.h
+++ setting_obj.h
@@ -2,46 +2,53 @@
 #define SETTING_OBJ_H_
 
 #include "Arduino.h"
+#include <String.h>
 
 enum settingTypes {
   SET_TYPE_BOOL,
   SET_TYPE_CNTR,
-  SET_TYPE_SRING,
+  SET_TYPE_STRING,
   NUM_SETTING_TYPES
-};
+  };
 
 class setting_obj {
-  public:
-     String soName;
-     String soDesc;
-     uint8_t soType;  // bool, cntr, string
-     bool soReadOnly;
+public:
+  String soName;
+  String soDesc;
+  uint8_t soType;  // bool, cntr, string
+  bool soReadOnly;
 
-     uint8_t soBool;
-     uint16_t soCntr;
-     String soString;
+  uint8_t soBool;
+  uint16_t soCntr;
+  String soString;
 
-     setting_obj();
+  setting_obj();
 
-     setting_obj(String nName, uint8_t nValue, bool nReadOnly);
-     setting_obj(String nName, uint16_t nValue, bool nReadOnly);
-     setting_obj(String nName, String nValue, bool nReadOnly);
+  // ESPFlashCounter* soEspFlashCounterPtr;
+  // ESPFlashString* soEspFlashString;
 
-     setting_obj(String nName, String nDesc, uint8_t nValue, bool nReadOnly);
-     setting_obj(String nName, String nDesc, uint16_t nValue, bool nReadOnly);
-     setting_obj(String nName, String nDesc, String nValue, bool nReadOnly);
+  setting_obj(String nName, bool nValue, bool nReadOnly);
+  setting_obj(String nName, uint16_t nValue, bool nReadOnly);
+  setting_obj(String nName, String nValue, bool nReadOnly);
 
-     void soSetVal(uint8_t nValue);
-     void soSetVal(uint16_t nValue);
-     void soSetVal(String nValue);
-
-     uint8_t getBool();
-     uint16_t getCounter();
-     String getString();
-
-     String getValAsString();
+  // setting_obj(String nName, bool nValue, ESPFlashCounter* nEspFlashCntr, bool nReadOnly);
+  // setting_obj(String nName, uint16_t nValue, ESPFlashCounter* nEspFlashCntr, bool nReadOnly);
+  // setting_obj(String nName, String nValue, ESPFlashString* nEspFlashStr, bool nReadOnly);
 
 
-};
+  void soSetVal(bool nValue);
+  void soSetVal(uint16_t nValue);
+  void soSetVal(String nValue);
+
+  bool getBool();
+  uint16_t getCounter();
+  String getString();
+
+  String getValAsString();
+
+private:
+  void constructor_core();
+
+  };
 
 #endif
