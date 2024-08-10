9e6af1943a34ebade788cd350adac9fee3ad899b
Odie <odie@odiesystems.com>
2024-08-09 20:03:52 -0700

    update formatting

--- setting_obj.h
+++ setting_obj.h
@@ -12,43 +12,43 @@ enum settingTypes {
   };
 
 class setting_obj {
-public:
-  String soName;
-  String soDesc;
-  uint8_t soType;  // bool, cntr, string
-  bool soReadOnly;
+  public:
+    String soName;
+    String soDesc;
+    uint8_t soType;  // bool, cntr, string
+    bool soReadOnly;
 
-  uint8_t soBool;
-  uint16_t soCntr;
-  String soString;
+    uint8_t soBool;
+    uint16_t soCntr;
+    String soString;
 
-  setting_obj();
+    setting_obj();
 
-  // ESPFlashCounter* soEspFlashCounterPtr;
-  // ESPFlashString* soEspFlashString;
+    // ESPFlashCounter* soEspFlashCounterPtr;
+    // ESPFlashString* soEspFlashString;
 
-  setting_obj(String nName, bool nValue, bool nReadOnly);
-  setting_obj(String nName, uint16_t nValue, bool nReadOnly);
-  setting_obj(String nName, String nValue, bool nReadOnly);
+    setting_obj(String nName, bool nValue, bool nReadOnly);
+    setting_obj(String nName, uint16_t nValue, bool nReadOnly);
+    setting_obj(String nName, String nValue, bool nReadOnly);
 
-  // setting_obj(String nName, bool nValue, ESPFlashCounter* nEspFlashCntr, bool nReadOnly);
-  // setting_obj(String nName, uint16_t nValue, ESPFlashCounter* nEspFlashCntr, bool nReadOnly);
-  // setting_obj(String nName, String nValue, ESPFlashString* nEspFlashStr, bool nReadOnly);
+    // setting_obj(String nName, bool nValue, ESPFlashCounter* nEspFlashCntr, bool nReadOnly);
+    // setting_obj(String nName, uint16_t nValue, ESPFlashCounter* nEspFlashCntr, bool nReadOnly);
+    // setting_obj(String nName, String nValue, ESPFlashString* nEspFlashStr, bool nReadOnly);
 
 
-  void soSetVal(bool nValue);
-  void soSetVal(uint16_t nValue);
-  void soSetVal(String nValue);
+    void soSetVal(bool nValue);
+    void soSetVal(uint16_t nValue);
+    void soSetVal(String nValue);
 
-  bool getBool();
-  uint16_t getCounter();
-  String getString();
+    bool getBool();
+    uint16_t getCounter();
+    String getString();
 
-  String getValAsString();
+    String getValAsString();
 
-private:
-  void constructor_core();
+  private:
+    void constructor_core();
 
-  };
+};
 
 #endif
